770d681d071cd3c299f40d81b4a6d99ab048e4e3
Yacine <anykeyh@gmail.com>
2024-03-14 13:38:04 +0700

    Add documentation to field

--- lib/verse/schema/field.rb
+++ lib/verse/schema/field.rb
@@ -6,6 +6,7 @@ require_relative "./post_processor"
 
 module Verse
   module Schema
+    # A field in a schema
     class Field
       include Optionable
 
@@ -34,6 +35,36 @@ module Verse
         self
       end
 
+      # Add metadata to the field. Useful for documentation
+      # purpose
+      # @param [Hash] opts the fields to add to the meta hash
+      # @return [self]
+      # @example
+      #
+      #   field(:name, String).meta(description: "The name of the user")
+      #
+      def meta(**opts)
+        @opts[:meta] ||= {}
+        @opts[:meta].merge!(opts)
+
+        self
+      end
+
+      # Set the default value for the field
+      #
+      # Please note that the default value is applied BEFORE any post processor
+      # such as `transform` or `rule` are ran.
+      #
+      # For example, if you have this:
+      #
+      #   field(:age, Integer).default(17).rule("must be greater than 18") { |v| v > 18 }
+      #
+      # This will fail if the field is not present, because the default value
+      # is applied before the rule is ran.
+      #
+      # @param [Object] value the default value
+      # @param [Proc] block the block to call to get the default value, if any.
+      # @return [self]
       def default(value = Optionable::NOTHING, &block)
         if value == Optionable::NOTHING && !block_given?
           if @default.is_a?(Proc)
@@ -52,6 +83,7 @@ module Verse
         !!@has_default
       end
 
+      # Mark the field as required
       def required
         @opts[:optional] = false
         @has_default = false
@@ -67,6 +99,16 @@ module Verse
         !required?
       end
 
+      # Add a rule to the field. A rule is a block that will be called
+      # with the value of the field. If the block returns false, an error
+      # will be added to the error builder.
+      #
+      # `rule` and `transform` can be chained together to add multiple rules.
+      # They are called in the order they are added.
+      #
+      # @param [String] error message if the rule is failing
+      # @param [Proc] block the block to call to validate the value
+      # @return [self]
       def rule(rule, &block)
         rule_processor = \
           case rule
@@ -94,12 +136,19 @@ module Verse
         self
       end
 
+      # Add a transformation to the field. A transformation is a block that
+      # will be called with the value of the field. The return value of the
+      # block will be the new value of the field.
+      #
+      # If the block raises an error, the error will be added to the error
+      # builder.
       def transform(&block)
         @post_processors.attach(
           PostProcessor.new(&block)
         )
       end
 
+      # :nodoc:
       def apply(value, output, error_builder)
         if @type.is_a?(Base)
           if value.is_a?(Hash)
