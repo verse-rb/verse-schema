0c26e7d149d207b591c116cd35d503d5308454f0
Yacine <anykeyh@gmail.com>
2025-04-13 19:01:45 +0700

    remove unused code

--- lib/verse/schema/selector.rb
+++ lib/verse/schema/selector.rb
@@ -20,18 +20,6 @@ module Verse
         @values    = values.transform_values{ |v| v.is_a?(Array) ? v : [v] }
       end
 
-      def transform(&block)
-        callback = proc do |value, error_builder|
-          stop if error_builder.errors.any?
-          instance_exec(value, error_builder, &block)
-        end
-
-        @post_processors ||= IDENTITY_PP.dup
-        @post_processors.attach(
-          PostProcessor.new(&callback)
-        )
-      end
-
       def validate(input, error_builder: nil, locals: {})
         locals = locals.dup # Ensure they are not modified
 
